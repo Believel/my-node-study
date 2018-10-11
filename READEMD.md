12e4d2ddc19399bd06a69357b669c178d743235f
tgd <zjrzpp0414@163.com>
2018-10-11 18:27:18 +0800

    update

---
+++ READEMD.md
@@ -0,0 +1,7 @@
+## 项目(express + mongoose)
+> 模板引擎：jade, 基于Node.js的开发web的框架：express
+## 项目启动
+```
+npm install
+node app.js
+```
--- app.js
+++ app.js
@@ -9,7 +9,10 @@ app.set('views', './views')
 app.set('view engine', 'jade')
 
 // index page
+// 给视图文件定义路由
 app.get('/', function(req, res){
+    // 第一个参数是视图文件名
+    // 第二个参数是给这个视图文件传的参数对象
     res.render('index', {
         title: 'imooc 首页'
     })
